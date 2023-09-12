6b2910578fff3cc0ba74e609de1a0140f14f9043
11874 <1187491052@qq.com>
2023-09-12 14:40:09 +0800

    Initial commit

---
+++ src/main/java/com/example/prosettingsshareservice/ProSettingsShareServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.prosettingsshareservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProSettingsShareServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProSettingsShareServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/prosettingsshareservice/ProSettingsShareServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.prosettingsshareservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProSettingsShareServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
